81ace1cef0ca61b6f28de49e9f82d0312c5053b0
Shota Shimazu <hornet.live.mf@gmail.com>
2020-12-18 10:51:23 +0900

    fix error?

--- Formula/basictex-universal.rb
+++ Formula/basictex-universal.rb
@@ -1,10 +1,10 @@
 class BasictexUniversal < Formula
-  version "2020"
-  sha256 "3b6080e94b7a8e3fa07e55e844e74afa6650ced0a27ce90b2d87855f23750b82s"
+
   url "https://www.tug.org/mactex/BasicTeX-2020-Universal.pkg"
   name "BasicTeXUniversal"
   desc "Compact TeX distribution as alternative to the full TeX Live / MacTeX"
   homepage "https://www.tug.org/mactex/aboutarm.html"
+  sha256 "3b6080e94b7a8e3fa07e55e844e74afa6650ced0a27ce90b2d87855f23750b82s"
 
 
   depends_on macos: ">= :big_sur", cask: "basictex-base"
@@ -14,11 +14,18 @@ class BasictexUniversal < Formula
     "basictex",
   ]
 
+  args1 = %W[
+    --expand
+    BasicTeX-2020-Universal.pkg
+    ./tmp/
+  ]
+
   def install
-    system 'pkgutil', '--expand', 'BasicTeX-2020-Universal.pkg', './tmp/'
-    system 'pax', '-rz', '-f', './tmp/BasicTeX-2020-Universal-Start.pkg/Payload'
-    system 'mv', 'usr/local/texlive/2020/bin/custom', '/usr/local/texlive/2020/bin/'
+    system "pkgutil", *args1
+    system "pax", "-rz", "-f", "./tmp/BasicTeX-2020-Universal-Start.pkg/Payload"
+    system "mv", "./tmp/usr/local/texlive/2020/bin/custom", "/usr/local/texlive/2020/bin/"
   end
+
   def caveats
     <<~EOS
       This is unofficial version Tex.
