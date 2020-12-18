97d513f9e383834c111ffb2664af1722672c1078
Shota Shimazu <hornet.live.mf@gmail.com>
2020-12-18 10:21:26 +0900

    revise

--- Formula/basictex-universal.rb
+++ Formula/basictex-universal.rb
@@ -1,4 +1,4 @@
-class Basictexuniversal < Formula
+class BasictexUniversal < Formula
   version "2020"
   sha256 "3b6080e94b7a8e3fa07e55e844e74afa6650ced0a27ce90b2d87855f23750b82s"
   url "https://www.tug.org/mactex/BasicTeX-2020-Universal.pkg"
@@ -16,5 +16,14 @@ class Basictexuniversal < Formula
     system 'pkgutil', '--expand', 'BasicTeX-2020-Universal.pkg', './tmp/'
     system 'pax', '-rz', '-f', .'./tmp/BasicTeX-2020-Universal-Start.pkg/Payload'
     system 'mv', 'usr/local/texlive/2020/bin/custom', '/usr/local/texlive/2020/bin/'
+  end
+  def caveats
+    <<~EOS
+      This is unofficial version Tex.
+    EOS
+  end
+
+  test do
+    return true
   end
 end
