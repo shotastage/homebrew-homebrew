4c47483e84924bdba26113fdfe0762bdc7ad3790
Shota Shimazu <shotastage@users.noreply.github.com>
2020-12-29 09:48:58 +0900

    test

--- Formula/basictex-universal.rb
+++ Formula/basictex-universal.rb
@@ -2,7 +2,7 @@ class BasictexUniversal < Formula
   desc "Compact TeX distribution as alternative to the full TeX Live / MacTeX"
   homepage "https://www.tug.org/mactex/aboutarm.html"
   url "https://www.tug.org/mactex/BasicTeX-2020-Universal.pkg"
-  sha256 "3b6080e94b7a8e3fa07e55e844e74afa6650ced0a27ce90b2d87855f23750b82s"
+  sha256 "3b6080e94b7a8e3fa07e55e844e74afa6650ced0a27ce90b2d87855f23750b82"
 
 
 
@@ -13,7 +13,6 @@ class BasictexUniversal < Formula
   ]
 
   def install
-    system "sl"
     system "pkgutil", *args1
     system "pax", "-rz", "-f", "./tmp/BasicTeX-2020-Universal-Start.pkg/Payload"
     system "mv", "./tmp/usr/local/texlive/2020/bin/custom", "/usr/local/texlive/2020/bin/"
