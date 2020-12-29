fe2a7c4fcdaa1aeea4f9abc1883bc322fefd226f
Shota Shimazu <shotastage@users.noreply.github.com>
2020-12-29 09:49:57 +0900

    revise

--- Formula/basictex-universal.rb
+++ Formula/basictex-universal.rb
@@ -13,7 +13,7 @@ class BasictexUniversal < Formula
   ]
 
   def install
-    system "pkgutil", *args1
+    system "pkgutil", "--expand", "BasicTeX-2020-Universal.pkg", "./tmp/"
     system "pax", "-rz", "-f", "./tmp/BasicTeX-2020-Universal-Start.pkg/Payload"
     system "mv", "./tmp/usr/local/texlive/2020/bin/custom", "/usr/local/texlive/2020/bin/"
   end
