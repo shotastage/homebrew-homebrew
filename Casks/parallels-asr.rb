5417fb1f6acd4b3ec46cc90ecf5cb27016fd93a5
Shota Shimazu <shotastage@users.noreply.github.com>
2021-01-04 12:01:45 +0900

    add parallels

---
+++ Casks/parallels-asr.rb
@@ -0,0 +1,59 @@
+# Based on: https://github.com/Homebrew/homebrew-cask/blob/HEAD/Casks/parallels.rb
+cask "parallels-asr" do
+  version "16.3.1-50393"
+  sha256 "790d2c09b6abb38506e00d3d376d763959a0d2c289b6a23d5b410239d7901f46"
+
+  url "https://download.parallels.com/desktop/v#{version.major}/#{version}/ParallelsDesktop-#{version}.dmg"
+  appcast "https://kb.parallels.com/en/125053"
+  name "Parallels Desktop"
+  desc "Desktop virtualization software"
+  homepage "https://www.parallels.com/products/desktop/"
+
+  auto_updates true
+  depends_on macos: ">= :arm64_big_sur"
+
+  app "Parallels Desktop.app"
+
+  preflight do
+    system_command "chflags",
+                   args: ["nohidden", "#{staged_path}/Parallels Desktop.app"]
+    system_command "xattr",
+                   args: ["-d", "com.apple.FinderInfo", "#{staged_path}/Parallels Desktop.app"]
+  end
+
+  postflight do
+    system_command "#{appdir}/Parallels Desktop.app/Contents/MacOS/inittool",
+                   args: ["init"],
+                   sudo: true
+  end
+
+  uninstall_preflight do
+    set_ownership "#{appdir}/Parallels Desktop.app"
+  end
+
+  uninstall delete: [
+    "/usr/local/bin/prl_convert",
+    "/usr/local/bin/prl_disk_tool",
+    "/usr/local/bin/prl_perf_ctl",
+    "/usr/local/bin/prlcore2dmp",
+    "/usr/local/bin/prlctl",
+    "/usr/local/bin/prlexec",
+    "/usr/local/bin/prlsrvctl",
+  ]
+
+  zap trash: [
+    "~/.parallels_settings",
+    "~/Library/Caches/com.apple.helpd/Generated/com.parallels.desktop.console.help*",
+    "~/Library/Caches/com.parallels.desktop.console",
+    "~/Library/Caches/Parallels Software/Parallels Desktop",
+    "~/Library/Logs/parallels.log",
+    "~/Library/Parallels/Parallels Desktop",
+    "~/Library/Preferences/com.parallels.desktop.console.LSSharedFileList.plist",
+    "~/Library/Preferences/com.parallels.desktop.console.plist",
+    "~/Library/Preferences/com.parallels.Parallels Desktop Statistics.plist",
+    "~/Library/Preferences/com.parallels.Parallels Desktop Events.plist",
+    "~/Library/Preferences/com.parallels.Parallels Desktop.plist",
+    "~/Library/Preferences/com.parallels.Parallels.plist",
+    "~/Library/Preferences/com.parallels.PDInfo.plist",
+  ]
+end
