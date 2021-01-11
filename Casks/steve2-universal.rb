f1f9f142d58042725407fe2b37969513d571061b
Shota Shimazu <shotastage@users.noreply.github.com>
2021-01-11 14:42:59 +0900

    add steve2

---
+++ Casks/steve2-universal.rb
@@ -0,0 +1,13 @@
+cask "steve2-universal" do
+  version "0.85"
+  sha256 "4ae85f660d6d02da74ce9390d6a22c6ccbedb7790f612542bde3a1c1332b434e"
+  url "https://digital-nichan.com/dlcenter/macos/universal/Steve2.zip"
+  name "Steve2"
+  desc "Steve ][ - Speedy Apple ][ Emulator"
+  homepage "https://github.com/trudnai/Steve2"
+  
+  app "Steve ][.app"
+  
+  zap trash: [
+  ]
+end
