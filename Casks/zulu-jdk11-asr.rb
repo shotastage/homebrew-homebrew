573d67b13b6d82af50efc49ed077903c08b29543
Shota Shimazu <shotastage@users.noreply.github.com>
2021-01-21 14:29:08 +0900

    revise

--- Casks/zulu-jdk11-asr.rb
+++ Casks/zulu-jdk11-asr.rb
@@ -1,6 +1,6 @@
 cask "zulu-jdk11-asr" do
-    version "15.28.1013,15.0.1"
-    sha256 "055a493236ed0023216f40d4e222ebc9cebd13bcaa8288d9f6c6c5c2d61f30ee"
+    version "11.43.1021,11.0.9.1"
+    sha256 "260a9d1bd3acda5b82bd3d61096fb3eec9985e5c37817ee2d3bb21f1134f0b36"
   
     url "https://cdn.azul.com/zulu/bin/zulu#{version.before_comma}-ca-jdk#{version.after_comma}-macosx_aarch64.dmg"
     name "Zulu Builds of OpenJDK"
@@ -8,7 +8,7 @@ cask "zulu-jdk11-asr" do
   
     depends_on macos: ">= :arm64_big_sur"
   
-    pkg "Double-Click to Install Zulu 15.pkg"
+    pkg "Double-Click to Install Zulu 11.pkg"
 
     uninstall pkgutil: "com.azulsystems.zulu.#{version.major}",
             delete:  [
