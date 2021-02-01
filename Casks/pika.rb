3e77783aa6d79009abe5fa74741a92b8d093c913
Shota Shimazu <shotastage@users.noreply.github.com>
2021-02-01 22:08:45 +0900

    update url

--- Casks/pika.rb
+++ Casks/pika.rb
@@ -4,7 +4,7 @@ cask "pika" do
   url "https://github-releases.githubusercontent.com/325284147/f0a70800-5d74-11eb-96ca-43d594c9386a?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20210201%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210201T004229Z&X-Amz-Expires=300&X-Amz-Signature=797f2b681c61af75a86596b2a877ae75328c61aa6167da72de6db3efa57e00d3&X-Amz-SignedHeaders=host&actor_id=3389538&key_id=0&repo_id=325284147&response-content-disposition=attachment%3B%20filename%3DPika-0.0.8.dmg&response-content-type=application%2Foctet-stream"
   name "Pika"
   desc "Pika (pronounced pi·kuh, like picker) is an easy to use, open-source, native colour picker for macOS. Pika makes it easy to quickly find colours onscreen, in the format you need, so you can get on with being a speedy, successful designer."
-  homepage "https://github.com/trudnai/Steve2"
+  homepage "https://github.com/superhighfives/pika"
   
   app "Pika.app"
   
