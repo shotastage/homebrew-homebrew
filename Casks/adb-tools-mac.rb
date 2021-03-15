58b8df44287b661882744adff6dceda0c42fd152
Shota Shimazu <shotastage@users.noreply.github.com>
2021-03-16 00:37:44 +0900

    Add adb connect app

---
+++ Casks/adb-tools-mac.rb
@@ -0,0 +1,22 @@
+cask "adb-tools-mac" do
+    version "1.1"
+    sha256 "712290d66504b3d8ac7a465b113e979006b3cb314a4b1c1fe626f7dba1af5be5"
+  
+    url "https://github.com/naman14/adb-tools-mac/releases/download/V1.1/adbconnect.app.zip"
+
+    name "adb connect mac"
+    desc "adb-tools-mac is a macOS menu bar app written in SwiftUI for common adb tools."
+    homepage "https://github.com/naman14/adb-tools-mac"
+
+    depends_on macos: ">= :arm64_big_sur"
+
+    app "adbconnect.app"
+
+    zap trash: [
+      "~/Library/Android/sdk",
+    ],
+        rmdir: [
+          "~/AndroidStudioProjects",
+          "~/Library/Android",
+        ]
+end
